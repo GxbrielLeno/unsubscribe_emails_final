1185842d8f47020d339d9b61330bc361539d9056
Gabriel Leno <gabrielleno.p@gmail.com>
2023-12-14 13:03:07 -0300

    meu primeiro commit

--- src/app.js
+++ src/app.js
@@ -1,39 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const cors = require('cors'); // Adicione esta linha
+const fs = require('fs').promises;
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors()); // Adicione esta linha para habilitar o CORS
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const csvWriter = createCsvWriter({
-  path: 'unsubscribe_emails.csv',
-  header: [
-    { id: 'email', title: 'Email' },
-  ],
-});
+const emailsFilePath = '/tmp/unsubscribe_emails.json';
 
-app.post('/unsubscribe', (req, res) => {
+app.post('/unsubscribe', async (req, res) => {
   const userEmail = req.body.email;
 
   if (userEmail) {
-    csvWriter.writeRecords([{ email: userEmail }])
-      .then(() => {
-        console.log(`E-mail ${userEmail} adicionado ao arquivo CSV.`);
-        res.send('Você foi removido da lista de e-mails.');
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Erro ao processar a solicitação.');
-      });
+    try {
+      // Lê os e-mails já existentes, se houver
+      const existingEmails = JSON.parse(await fs.readFile(emailsFilePath, 'utf-8'));
+
+      // Adiciona o novo e-mail à lista
+      existingEmails.push({ email: userEmail });
+
+      // Salva a lista atualizada no arquivo
+      await fs.writeFile(emailsFilePath, JSON.stringify(existingEmails));
+
+      console.log(`E-mail ${userEmail} adicionado à lista.`);
+      res.send('Você foi removido da lista de e-mails.');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Erro ao processar a solicitação.');
+    }
   } else {
     res.status(400).send('Endereço de e-mail inválido.');
   }
 });
 
-app.listen(3333, () => {
-  console.log(`rodando`);
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
